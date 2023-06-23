6abe7ebf2005f6bf962a4bc241bd89f35466dcfa
Arthur Vergaças <arthurvdm@gmail.com>
2023-06-23 19:44:21 -0300

    feaT: cria função wrapper execute

--- core/connection.py
+++ core/connection.py
@@ -1,3 +1,4 @@
+from typing import Any, Optional
 import oracledb
 
 
@@ -9,6 +10,20 @@ class DBConnection:
             DBConnection.connection = self.__create_connection(
                 db_data_source, db_user, db_password
             )
+
+    def execute(
+        self, statement: str, parameters: Optional[dict[Any, Any]] = None
+    ) -> oracledb.Cursor | None:
+        if DBConnection.connection is None:
+            raise Exception(
+                "You must instantiate DBConnection before executing SQL statements"
+            )
+
+        result_cursor: oracledb.Cursor | None =
+                DBConnection.connection.cursor().execute(statement, parameters)  # type: ignore # 😠
+
+        if result_cursor is not None:
+            return result_cursor
 
     def __create_connection(
         self, db_data_source: str, db_user: str, db_password: str
